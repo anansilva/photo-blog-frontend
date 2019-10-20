ea1147ddd0fd8e05dce1a5c6d33715712514a178
Ana Silva <ananunesdasilva@gmail.com>
2019-10-20 19:00:26 +0100

    Move dynamic navbar items to the right

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,13 @@ import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
-  toggleLogout = () => {
+  toggleNavItems = () => {
     if (this.props.currentUser && this.props.currentUser.token) {
       return (
-        <li className="nav-item"><NavLink exact className="nav-link" to="/logout">Log Out</NavLink></li>
-      )
-    }
-  }
-
-  toggleUploadPhoto = () => {
-    if (this.props.currentUser && this.props.currentUser.token) {
-      return (
-        <li className="nav-item"><NavLink exact className="nav-link" to="/posts/new">Upload photo</NavLink></li>
+        <ul class="nav navbar-nav navbar-right">
+          <li className="nav-item"><NavLink exact className="nav-link" to="/logout">Log Out</NavLink></li>
+          <li className="nav-item"><NavLink exact className="nav-link" to="/posts/new">Upload photo</NavLink></li>
+        </ul>
       )
     }
   }
@@ -24,9 +19,8 @@ class Navbar extends Component {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item"><NavLink exact className="nav-link" activeClassName="active" to="/">Photo Blog</NavLink></li>
-          {this.toggleLogout()}
-          {this.toggleUploadPhoto()}
          </ul>
+          {this.toggleNavItems()}
       </nav>
     );
   }
